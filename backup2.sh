c7a0bb74ddb35d6da33b3265bdbf3567b4eab9ea
AusCyber <willp@outlook.com.au>
2020-11-18 21:21:14 +1100

    Wed 18 Nov 09:21pm backup

--- backup2.sh
+++ backup2.sh
@@ -4,14 +4,13 @@ cp -r /etc/portage $DEST
 rsync -avhr /etc/portage/ portage/ --delete
 cp /usr/src/linux/.config $DEST/kernel/
 #zip -r ~/dotfiles/rice.zip  {~/.xmonad,~/st,~/dmenu,~/.xinitrc,~/.Xresources}
-rsync -avhr ~/.xmonad $DEST --delete
-rsync -avhr ~/st $DEST --delete --exclude .git
-rsync -avhr ~/.config/polybar $DEST --delete
+rsync -avhr ~/.xmonad $DEST/rice --delete
+rsync -avhr ~/st $DEST/rice --delete --exclude .git
+rsync -avhr ~/.config/polybar/rice $DEST --delete
 rsync -avhr ~/.config/nvim $DEST --delete
-rsync -avhr ~/st $DEST --delete
-rsync -avhr ~/st $DEST --delete
-cp ~/.xinitrc $DEST 
-cp ~/.Xresources $DEST
+cp ~/.xinitrc $DEST/rice/
+cp ~/.Xresources $DEST/rice/
+cp ~/.zshrc $DEST
 cd ~/dotfiles
 git add ./*
 git commit -a -m "$(date "+%a %d %b %I:%M%P") backup"
