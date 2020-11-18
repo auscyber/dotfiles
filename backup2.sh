69c88e74d5d65c260059e1749c7e94339dc8b5c7
AusCyber <willp@outlook.com.au>
2020-11-18 21:31:03 +1100

    Wed 18 Nov 09:31pm backup

--- backup2.sh
+++ backup2.sh
@@ -1,13 +1,15 @@
 HOME=/home/auscyber
 DEST=~/dotfiles 
-cp -r /etc/portage $DEST
-rsync -avhr /etc/portage/ portage/ --delete
+rsync -avhr /etc/portage/ portage/etc/ --delete
+rsync -avh /var/lib/portage/world portage/world --delete
 cp /usr/src/linux/.config $DEST/kernel/
 #zip -r ~/dotfiles/rice.zip  {~/.xmonad,~/st,~/dmenu,~/.xinitrc,~/.Xresources}
 rsync -avhr ~/.xmonad $DEST/rice --delete
 rsync -avhr ~/st $DEST/rice --delete --exclude .git
-rsync -avhr ~/.config/polybar/rice $DEST --delete
+rsync -avhr ~/.config/polybar $DEST/rice --delete
 rsync -avhr ~/.config/nvim $DEST --delete
+rsync -avhr ~/.config/picom $DEST --delete
+rsync -avhr ~/.config/rofi $DEST/rice --delete
 cp ~/.xinitrc $DEST/rice/
 cp ~/.Xresources $DEST/rice/
 cp ~/.zshrc $DEST
