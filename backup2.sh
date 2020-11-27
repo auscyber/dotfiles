0c51d912a5f87043b3aac45956ef2b1d23e7f188
AusCyber <willp@outlook.com.au>
2020-11-27 21:02:23 +1100

    Fri 27 Nov 09:02pm backup

--- backup2.sh
+++ backup2.sh
@@ -4,7 +4,7 @@ rsync -avhr /etc/portage/ portage/etc/ --delete
 rsync -avh /var/lib/portage/world portage/world --delete
 cp /usr/src/linux/.config $DEST/kernel/
 #zip -r ~/dotfiles/rice.zip  {~/.xmonad,~/st,~/dmenu,~/.xinitrc,~/.Xresources}
-rsync -avhr ~/.xmonad $DEST/rice --delete
+rsync -avhr ~/.xmonad $DEST/rice --delete --exclude *.o
 rsync -avhr ~/st $DEST/rice --delete --exclude .git
 rsync -avhr ~/.config/polybar $DEST/rice --delete
 rsync -avhr ~/.config/nvim $DEST --delete
@@ -14,7 +14,7 @@ cp ~/.xinitrc $DEST/rice/
 cp ~/.Xresources $DEST/rice/
 cp ~/.zshrc $DEST
 cd ~/dotfiles
-git add -all
+git add -all --exclude *.o
 git commit -a -m "$(date "+%a %d %b %I:%M%P") backup"
 git push origin master
 
