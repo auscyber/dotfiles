ea84efea0ed0f3be376cb0689af4297d7bf929e5
AusCyber <willp@outlook.com.au>
2020-11-30 17:49:17 +1100

    Mon 30 Nov 05:49pm backup

--- backup2.sh
+++ backup2.sh
@@ -6,10 +6,10 @@ cp /usr/src/linux/.config $DEST/kernel/
 #zip -r ~/dotfiles/rice.zip  {~/.xmonad,~/st,~/dmenu,~/.xinitrc,~/.Xresources}
 rsync -avhr ~/.xmonad $DEST/rice --delete --exclude *.o
 rsync -avhr ~/st $DEST/rice --delete --exclude .git
-rsync -avhr ~/.config/polybar $DEST/rice --delete
-rsync -avhr ~/.config/nvim $DEST --delete
-rsync -avhr ~/.config/picom $DEST --delete
-rsync -avhr ~/.config/rofi $DEST/rice --delete
+rsync -avhr ~/.config/polybar $DEST/.config --delete
+rsync -avhr ~/.config/nvim $DEST/.config --delete
+rsync -avhr ~/.config/picom $DEST/.config --delete
+rsync -avhr ~/.config/rofi $DEST/.config --delete
 cp ~/.xinitrc $DEST/rice/
 cp ~/.Xresources $DEST/rice/
 cp ~/.zshrc $DEST
