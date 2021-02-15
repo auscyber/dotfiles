f2bc0226daf6387b3512d06f14d161f8cab74153
AusCyber <willp@outlook.com.au>
2021-02-15 11:32:14 +1100

    fixup! Mon 15 Feb 11:31am backup

--- .emacs.d/elpa/lsp-haskell-20201210.1702/lsp-haskell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/auscyber/.emacs.d/elpa/lsp-haskell-20201210.1702/lsp-haskell.el  -*- no-byte-compile: t -*-
-(define-package "lsp-haskell" "20201210.1702" "Haskell support for lsp-mode" '((lsp-mode "3.0") (haskell-mode "1.0")) :commit "592e883026288677f4042333b51efc7a40b9a68a" :keywords '("haskell") :url "https://github.com/emacs-lsp/lsp-haskell")
