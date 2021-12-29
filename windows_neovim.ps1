251a8392fc76e344c7586c4f46991f5954653442
Will Pierlot <willp@outlook.com.au>
2021-12-29 20:00:15 +1100

    install neovim windows

--- windows_neovim.ps1
+++ windows_neovim.ps1
@@ -1,26 +1,43 @@
-if ($env:TEMP == "")
-{
-    $env:TEMP = "/tmp"
-}
-Function New-TempFolder
-{
-    $TEMPFILE = New-TemporaryFile
-    Remove-Item -Force $TEMPFILE
-    return Join-Path $env:TEMP $TEMPFILE.Name
-}
-
-$DownloadLoc  = New-TemporaryFile
-$ExtractFolder = New-TempFolder
-$OutputFile = Join-Path $HOME "neovim"
-$BinFolder = Join-Path "$OutputFile" "bin"
-if (!$env:PATH.Split(';').Contains($BinFolder)) 
-{
-    
-}
-
-
-Invoke-WebRequest "https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip" -OutFile $DownloadLoc
-Expand-Archive -Path $DownloadLoc -DestinationPath $ExtractFolder
-
-Copy-Item -Recurse -Path "$ExtractFolder\Neovim\*" -Destination $OutputFile
-
+$ErrorActionPreference = "Stop"
+Function New-TempFolder
+{
+
+    $TEMPFILE = New-TemporaryFile
+
+    Remove-Item -Force $TEMPFILE
+
+    return Join-Path $env:TEMP $TEMPFILE.Name
+
+}
+
+$DownloadLoc  = New-TemporaryFile
+$RenamedFile = $DownloadLoc.BaseName + ".zip"
+
+$ExtractFolder = New-TempFolder
+
+$OutputFolder = Join-Path $HOME "neovim"
+
+$BinFolder = Join-Path "$OutputFolder" "bin"
+
+
+
+
+Invoke-WebRequest "https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip" -OutFile $DownloadLoc
+Write-Output "Downloading neovim"
+
+Rename-Item $DownloadLoc $RenamedFile
+
+Expand-Archive -Path "$(Join-Path $DownloadLoc.Directory $RenamedFile)" -DestinationPath $ExtractFolder
+
+
+Remove-Item -Recurse -Force $OutputFolder
+Copy-Item -Recurse -Force -Path "$ExtractFolder\Neovim" -Destination $OutputFolder
+
+$UserPath = [Environment]::GetEnvironmentVariable("Path",[System.EnvironmentVariableTarget]::User)
+Write-Output $UserPath
+
+if (!$UserPath.Split(";").Contains($BinFolder))
+{
+    Write-Output Updating Path
+    [Environment]::SetEnvironmentVariable("Path","$UserPath;$BinFolder",[System.EnvironmentVariableTarget]::User)
+}
