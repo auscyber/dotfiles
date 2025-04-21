3d84aa2f8d4cb9f1250f49ef5bdc0c853f34cbc8
Ivy Pierlot <ivyp@outlook.com.au>
2025-04-21 15:36:47 +1000

    fix dots

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Define colors from polybar config
+BLACK="0xff2A2A2A"
+WHITE="0xffD9D9D9"
+BLUE="0xff0000ff"
+GREEN="0xff00ff00"
+PINK="0xffffd1dc"
+YELLOW="0xffFFDB9E"
+PURPLE="0xff9591FF"
+GRAY="0xff707078"
---
+++ .config/sketchybar/plugins/battery.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Get battery percentage and charging status
+PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
+
+if [ "$CHARGING" != "" ]; then
+    # Charging - show charging icon
+    case ${PERCENTAGE} in
+        9[0-9]|100) ICON="   " ;;
+        [6-8][0-9]) ICON="   " ;;
+        [3-5][0-9]) ICON="   " ;;
+        [1-2][0-9]) ICON="   " ;;
+        *) ICON="   " ;;
+    esac
+else
+    # Discharging - show battery level icon
+    case ${PERCENTAGE} in
+        9[0-9]|100) ICON=" " COLOR=0xff82FFA2 ;;
+        [6-8][0-9]) ICON=" " COLOR=0xff82FFA2 ;;
+        [3-5][0-9]) ICON=" " COLOR=0xffFFFFFF ;;
+        [1-2][0-9]) ICON=" " COLOR=0xffFFAE8F ;;
+        *) ICON=" " COLOR=0xffFF6970 ;;
+    esac
+fi
+
+sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%" icon.color=${COLOR:-0xffffffff}
