29778e6d3cbc6ecda48f1e9d5c41a12573e7bc7e
Ivy Pierlot <ivyp@outlook.com.au>
2025-05-05 16:18:14 +1000

    wow changed

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -7,11 +7,12 @@ CHARGING=$(pmset -g batt | grep 'AC Power')
 if [ "$CHARGING" != "" ]; then
     # Charging - show charging icon
     case ${PERCENTAGE} in
-        9[0-9]|100) ICON="   " ;;
-        [6-8][0-9]) ICON="   " ;;
-        [3-5][0-9]) ICON="   " ;;
-        [1-2][0-9]) ICON="   " ;;
-        *) ICON="   " ;;
+        100) ICON=" 󱐋";;
+		9[0-9]) ICON=" 󱐋";;
+        [6-8][0-9]) ICON=" 󱊥󱐋" ;;
+        [3-5][0-9]) ICON=" 󱊤󱐋" ;;
+        [1-2][0-9]) ICON=" 󰢟󱐋" ;;
+        *) ICON=" 󱐋" ;;
     esac
 else
     # Discharging - show battery level icon
@@ -19,7 +20,7 @@ else
         9[0-9]|100) ICON=" " COLOR=0xff82FFA2 ;;
         [6-8][0-9]) ICON=" " COLOR=0xff82FFA2 ;;
         [3-5][0-9]) ICON=" " COLOR=0xffFFFFFF ;;
-        [1-2][0-9]) ICON=" " COLOR=0xffFFAE8F ;;
+        [1-2][0-9]) ICON="" COLOR=0xffFFAE8F ;;
         *) ICON=" " COLOR=0xffFF6970 ;;
     esac
 fi
