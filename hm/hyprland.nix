cbd396442ac3b42dbf2eed49333d2ae47662afbf
Ivy Pierlot <ivyp@outlook.com.au>
2025-06-06 15:10:17 +1000

    fix hyprland

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -52,7 +52,24 @@
               vibrancy = 0.1696
           }
       }
+general {
+    gaps_in = 5
+    gaps_out = 20
 
+    border_size = 2
+
+    # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+    col.inactive_border = rgba(595959aa)
+
+    # Set to true enable resizing windows by clicking and dragging on borders and gaps
+    resize_on_border = false
+
+    # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+    allow_tearing = false
+
+    layout = dwindle
+}
 
     '';
     settings = {
@@ -65,7 +82,7 @@
       bind =
         [
           "$mod, T, exec, ghostty"
-          "$mod, C, exec, google-chrome-stable"
+          "$mod, B, exec, google-chrome-stable"
           "$mod, F, exec, dolphin"
           "$mod, C, killactive"
 
