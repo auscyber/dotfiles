bb36a774508b8230a80c1f299468d91b479861b4
Ivy Pierlot <ivyp@outlook.com.au>
2025-09-05 16:32:36 +1000

    change to lix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,16 @@
       url = "github:Crackerfracks/synesthaxia.glsl";
       flake = false;
     };
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
     ghostty-shaders = {
       url = "github:hackr-sh/ghostty-shaders";
       flake = false;
