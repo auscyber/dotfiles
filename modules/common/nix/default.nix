501f698fc47fda75778466eca94d9f2fa643b837
Ivy Pierlot <ivyp@outlook.com.au>
2025-11-14 14:26:19 +1100

    fix modules

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -32,6 +32,7 @@ in
           "https://devenv.cachix.org"
         ];
         trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
@@ -48,7 +49,7 @@ in
         options = "--delete-older-than 30d";
       };
 
-      #      package = pkgs.nixVersions.latest;
+      package = pkgs.nixVersions.stable;
     };
 
     nixpkgs.config = {
