0d45fde2851b9a6b07c35da3650d6ed52c4884c9
Ivy Pierlot <ivyp@outlook.com.au>
2025-12-30 16:06:36 +1100

    more stuff

--- homes/x86_64-linux/ivy@vmi1472413.contaboserver.net/default.nix
+++ homes/x86_64-linux/ivy@vmi1472413.contaboserver.net/default.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 {
   sops.age.keyFile = "/home/ivy/.config/sops/age/keys.txt";
+  targets.genericLinux.enable = true;
   auscybernix = {
+  nix.flake = "/home/ivy/dotfiles";
     secrets.enable = true;
     shell = {
       enable = true;
