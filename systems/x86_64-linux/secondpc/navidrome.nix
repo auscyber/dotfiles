327cc0521a7342002968842708bf78e4f248d4dd
Ivy Pierlot <ivyp@outlook.com.au>
2026-01-06 17:25:15 +1100

    cmon

--- systems/x86_64-linux/secondpc/navidrome.nix
+++ systems/x86_64-linux/secondpc/navidrome.nix
@@ -5,20 +5,21 @@ let
 in
 {
   age.secrets.slskd_secrets_env = {
-  rekeyFile = ../../../secrets/slsk_creds.env.age;
+    rekeyFile = ../../../secrets/slsk_creds.env.age;
 
-intermediary = true;
+    intermediary = true;
   };
   age.secrets.navidrome_env = {
-  rekeyFile = ./navidrome.age;
+    rekeyFile = ./navidrome.age;
 
   };
   systemd.tmpfiles.settings.music = {
-  "/mnt/hdd/Music/Downloads"."d" = {
-  user = ":music";
-  group = ":music";
-  mode = ":770";
+    "/mnt/hdd/Music/Downloads"."d" = {
+      user = ":music";
+      group = ":music";
+      mode = ":770";
 
+    };
   };
 
   age.secrets."slskd.env" = {
@@ -37,10 +38,10 @@ intermediary = true;
           ...
         }:
         ''
-          	printf 'SLSKD_USERNAME=ivy\n'
-          	printf 'SLSKD_PASSWORD=%s\n' $(${decrypt} ${lib.escapeShellArg deps.ivy-password.file})
-			${decrypt} ${lib.escapeShellArg deps.slskd_secrets_env.file}
-          	'';
+                    	printf 'SLSKD_USERNAME=ivy\n'
+                    	printf 'SLSKD_PASSWORD=%s\n' $(${decrypt} ${lib.escapeShellArg deps.ivy-password.file})
+          			${decrypt} ${lib.escapeShellArg deps.slskd_secrets_env.file}
+                    	'';
 
     };
 
@@ -48,36 +49,38 @@ intermediary = true;
   users.groups.music = {
   };
   users.users.music = {
-   isSystemUser = true;
-   group = "music";
+    isSystemUser = true;
+    group = "music";
 
   };
 
   services.navidrome = {
     enable = true;
-	environmentFile = config.age.secrets.navidrome_env.path;
-	group = "music";
+    environmentFile = config.age.secrets.navidrome_env.path;
+    group = "music";
     settings = {
-	user = "music";
+      user = "music";
       MusicFolder = "${path}";
     };
   };
   services.lidarr = {
     enable = true;
-	user = "music";
+    user = "music";
   };
 
   services.slskd = {
     enable = true;
     openFirewall = true;
-	user = "music";
+    user = "music";
     environmentFile = config.age.secrets."slskd.env".path;
     settings = {
       shares.directories = [ "${path}" ];
-	  directories.downloads = "/mnt/hdd/Music/Downloads";
+      directories.downloads = "/mnt/hdd/Music/Downloads";
     };
     domain = "slsk.ivymect.in";
     nginx = {
+
+	forceSSL = true;
       useACMEHost = "ivymect.in";
 
     };
