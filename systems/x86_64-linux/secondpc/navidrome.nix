99230b650135cfb31aedbcf37f4504e453cfb411
Ivy Pierlot <ivyp@outlook.com.au>
2026-01-06 20:46:28 +1100

    fix url

--- systems/x86_64-linux/secondpc/navidrome.nix
+++ systems/x86_64-linux/secondpc/navidrome.nix
@@ -128,7 +128,7 @@ in
           ...
         }:
         ''
-						printf 'SLSKD_API_KEY=role=administrator;cidr=0.0.00.0/0,::/0;%s\n' $(${decrypt} ${lib.escapeShellArg deps.soularr_api_key.file})
+						printf 'SLSKD_API_KEY="role=Administrator;cidr=0.0.00.0/0,::/0;%s"\n' $(${decrypt} ${lib.escapeShellArg deps.soularr_api_key.file})
                     	printf 'SLSKD_USERNAME=ivy\n'
                     	printf 'SLSKD_PASSWORD=%s\n' $(${decrypt} ${lib.escapeShellArg deps.ivy-password.file})
           			${decrypt} ${lib.escapeShellArg deps.slskd_secrets_env.file}
