fe77353138b808f7a418a409ef28280a39f08262
Ivy Pierlot <ivyp@outlook.com.au>
2026-01-13 17:31:00 +1100

    explicit sed

--- modules/age.nix
+++ modules/age.nix
@@ -105,7 +105,7 @@ let
         echo "replacing placeholder ${dep.placeholder} in ${templateType.name}..."
         test -f "${dep.file}" || echo "[agenix] WARNING: dependency file ${dep.file} does not exist!"
 
-        sed -i.bak "s|${dep.placeholder}|$(${ageBin} --decrypt "''${IDENTITIES[@]}" ${lib.escapeShellArg dep.file})|g" "$TMP_FILE"
+        ${pkgs.gnused}/bin/sed -i.bak "s|${dep.placeholder}|$(${ageBin} --decrypt "''${IDENTITIES[@]}" ${lib.escapeShellArg dep.file})|g" "$TMP_FILE"
         rm -f "$TMP_FILE.bak"
       '')
     )}
