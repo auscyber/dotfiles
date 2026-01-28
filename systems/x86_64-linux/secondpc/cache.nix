3331681e8bb43ac19f429a65b01dfbef6d9dd6ba
Ivy Pierlot <ivyp@outlook.com.au>
2026-01-28 15:31:18 +1100

    attic

--- systems/x86_64-linux/secondpc/cache.nix
+++ systems/x86_64-linux/secondpc/cache.nix
@@ -1,11 +1,58 @@
 { config, pkgs, ... }:
 {
 
-  #  services.atticd = {
-  #    enable = true;
-  #    environmentFile = config.sops.secrets."attic_env".path;
-  #
-  #  };
+  age.secrets."attic_env" = {
+    rekeyFile = ./attic_env.age;
+    generator = {
+      script =
+        { pkgs, ... }:
+        ''
+          printf 'ATTIC_SERVER_TOKEN_RS256_SECRET_BASE64=%s\n' "$(${pkgs.openssl}/bin/openssl genrsa -traditional 4096 | base64 -w0)"
+        '';
+
+    };
+
+  };
+  services.nginx.virtualHosts."cache.ivymect.in" = {
+    useACMEHost = "*.ivymect.in";
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://localhost:8069";
+    };
+  };
+  services.atticd = {
+    enable = true;
+    environmentFile = config.age.secrets."attic_env".path;
+    settings = {
+      listen = "[::]:8069";
+
+      jwt = { };
+
+      # Data chunking
+      #
+      # Warning: If you change any of the values here, it will be
+      # difficult to reuse existing chunks for newly-uploaded NARs
+      # since the cutpoints will be different. As a result, the
+      # deduplication ratio will suffer for a while after the change.
+      chunking = {
+        # The minimum NAR size to trigger chunking
+        #
+        # If 0, chunking is disabled entirely for newly-uploaded NARs.
+        # If 1, all NARs are chunked.
+        nar-size-threshold = 64 * 1024; # 64 KiB
+
+        # The preferred minimum size of a chunk, in bytes
+        min-size = 16 * 1024; # 16 KiB
+
+        # The preferred average size of a chunk, in bytes
+        avg-size = 64 * 1024; # 64 KiB
+
+        # The preferred maximum size of a chunk, in bytes
+        max-size = 256 * 1024; # 256 KiB
+      };
+    };
+
+  };
   services.ncps = {
     enable = true;
     cache = {
