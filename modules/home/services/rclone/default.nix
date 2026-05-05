0d16ec3952285acbe4379ee2ae5ea2160e80d914
Ivy Pierlot <ivyp@outlook.com.au>
2026-05-05 16:32:28 +1000

    fix rclone

--- modules/home/services/rclone/default.nix
+++ modules/home/services/rclone/default.nix
@@ -1,28 +1,33 @@
-{pkgs, config, lib,...}
-let cfg = config.auscybernix.services.rclone;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.auscybernix.services.rclone;
 in
 {
 
-options.auscybernix.services.rclone = {
-  enable = lib.mkOption {
-	type = lib.types.bool;
-	default = false;
-	description = "Whether to enable the rclone service.";
-  };
+  options.auscybernix.services.rclone = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to enable the rclone service.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
 
-  	programs.rclone = {
-	enable = true;
-	remotes.gdrive = {
-		config = {
-			type = "drive";
+    programs.rclone = {
+      enable = true;
+      remotes.gdrive = {
+        config = {
+          type = "drive";
 
-		};
-	};
-	};
-
+        };
+      };
+    };
 
   };
 }
