5b50ca135efdd0e71a9df30eb925eb044d47112e
Ivy Pierlot <ivyp@outlook.com.au>
2026-07-23 15:35:12 +1000

    fix ci again

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "main:6VI0mVQzSGSA9gB81hwXWllbWrP7ybLaP1Jd3sPUBf4="
+      "main:DDQOp9cJP4w/1/oqfviAPG5H/6G8WxbZ/PuFPPdY6bI="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -121,7 +121,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    github-actions-nix.url = "github:synapdeck/github-actions-nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
