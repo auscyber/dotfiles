f5c170d829fee731e7e6bcfce789f72cbaed7467
Ivy Pierlot <ivyp@outlook.com.au>
2026-07-24 23:00:57 +1000

    not overlays everywhere

--- packages/proton-ge.nix
+++ packages/proton-ge.nix
@@ -5,39 +5,42 @@
     fetch.tarball = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/$ver/$ver.tar.gz";
   };
 
-  den.aspects.packages.proton-ge-bin.overlays = { sources, ... }: {
-    proton-ge-bin = self: super: {
-      wrapProtonGE =
-        let
-          toPySettings =
-            attrs:
-            "user_settings = {\n"
-            + lib.concatStringsSep "\n" (
-              lib.mapAttrsToList (k: v: "    ${builtins.toJSON k}: ${builtins.toJSON v},") attrs
-            )
-            + "\n}\n";
+  den.aspects.packages.proton-ge-bin = {
 
-          build =
-            {
-              geProton,
-              settings ? { },
-            }:
-            super.runCommand "${geProton.name}-with-settings" { } ''
-              mkdir -p $out
-              cp -r ${geProton}/* $out/
-              chmod -R u+w $out
-              cat >$out/user_settings.py <<'EOF'
-              ${toPySettings settings}
-              EOF
-            '';
-        in
-        lib.makeOverridable build;
+    overlays = { sources, ... }: {
+      proton-ge-bin = self: super: {
+        wrapProtonGE =
+          let
+            toPySettings =
+              attrs:
+              "user_settings = {\n"
+              + lib.concatStringsSep "\n" (
+                lib.mapAttrsToList (k: v: "    ${builtins.toJSON k}: ${builtins.toJSON v},") attrs
+              )
+              + "\n}\n";
 
-      proton-ge-bin = super.proton-ge-bin.overrideAttrs { inherit (sources.proton-ge-bin) src version; };
+            build =
+              {
+                geProton,
+                settings ? { },
+              }:
+              super.runCommand "${geProton.name}-with-settings" { } ''
+                mkdir -p $out
+                cp -r ${geProton}/* $out/
+                chmod -R u+w $out
+                cat >$out/user_settings.py <<'EOF'
+                ${toPySettings settings}
+                EOF
+              '';
+          in
+          lib.makeOverridable build;
+
+        proton-ge-bin = super.proton-ge-bin.overrideAttrs { inherit (sources.proton-ge-bin) src version; };
+      };
     };
 
     nixos =
-      { pkgs, config, ... }:
+      { config, pkgs, ... }:
       let
         cfg = config.programs.steam.proton-ge;
       in
@@ -72,7 +75,7 @@
             settings = cfg.settings;
           };
           programs.steam.extraCompatPackages = [
-            (config.programs.steam.proton-ge.wrappedPackage)
+            cfg.wrappedPackage
           ];
 
         };
